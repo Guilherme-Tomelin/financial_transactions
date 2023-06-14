0684ded97f00194fba0c44bf26986f87e8e208f8
Guilherme-Tomelin <guilhermesantosgts@outlook.com>
2023-06-14 14:29:00 -0300

    Adicionada leitura de texto

--- galeria/Controller.py
+++ galeria/Controller.py
@@ -2,7 +2,27 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.http import HttpResponse
 import os
+import csv
 
+
+"""
+O arquivo controller.py é um módulo que contém a definição da classe 
+Controller e métodos relacionados que representam a lógica de controle 
+da sua aplicação. Ele é usado para organizar e centralizar a lógica de 
+negócios do seu aplicativo Django.
+"""
+
+"""
+Arquivo CSV é composto por:
+Banco origem
+Agência origem
+Conta origem
+Banco destino
+Agência destino
+Conta destino
+Valor da transação
+Data e hora da transação
+"""
 
 class Controller:
     def importar_arquivo(request):
@@ -11,5 +31,8 @@ class Controller:
         tamanho_arquivo = arquivo.size / (1024 * 1024)  # arquivo.size retorna tamanho em bytes e calcula convertendo para megabytes
         print(f"Arquivo importado: {nome_arquivo}")
         print(f"Tamanho do arquivo: {tamanho_arquivo} MB")
+
+        conteudo_arquivo = arquivo.read()
+        print(conteudo_arquivo)
 
         return HttpResponse()
